5c6afd80cd849de238ec05bc89bf3259f7f636a4
Ben Williams <43639041+BenWilliams1@users.noreply.github.com>
2021-03-19 20:08:39 +0000

    Added project to repo + mobile assets folder

---
+++ app/src/main/java/com/example/spyro2100walkthrough/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.spyro2100walkthrough.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
