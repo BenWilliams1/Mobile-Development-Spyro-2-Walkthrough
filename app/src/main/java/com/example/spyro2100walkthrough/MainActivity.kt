6e8f1fa214ea351db877a96d490754549634dd98
Ben Williams <43639041+BenWilliams1@users.noreply.github.com>
2021-05-01 03:00:20 +0100

    adding bottom nav icons, trying to remove back button

--- app/src/main/java/com/example/spyro2100walkthrough/MainActivity.kt
+++ app/src/main/java/com/example/spyro2100walkthrough/MainActivity.kt
@@ -1,20 +1,13 @@
 package com.example.spyro2100walkthrough
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.Toast
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.fragment.app.FragmentManager
-import com.google.android.youtube.player.YouTubeInitializationResult
-import com.google.android.youtube.player.YouTubePlayer
-import com.google.android.youtube.player.YouTubePlayerSupportFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,7 +24,9 @@ class MainActivity : AppCompatActivity() {
             R.id.navigation_home,
             R.id.navigation_gameinfo,
             R.id.navigation_walkthrough,
-            R.id.navigation_secrets))
+            R.id.navigation_secrets
+        ))
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
--- app/src/main/java/com/example/spyro2100walkthrough/ui/levels/summer_forest/GlimmerFragment.kt
+++ app/src/main/java/com/example/spyro2100walkthrough/ui/levels/summer_forest/GlimmerFragment.kt
@@ -1,20 +1,27 @@
 package com.example.spyro2100walkthrough.ui.levels.summer_forest
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.activity.OnBackPressedCallback
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.example.spyro2100walkthrough.R
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
 import com.google.android.youtube.player.YouTubePlayerSupportFragment
 import com.google.android.youtube.player.YouTubeStandalonePlayer
 
-class GlimmerFragment : Fragment() {
+class GlimmerFragment : Fragment(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
        // val youtubefragment = childFragmentManager.findFragmentById(R.id.youtube_fragment)
        //         as YouTubePlayerSupportFragment
 //
@@ -44,13 +51,30 @@ class GlimmerFragment : Fragment() {
 //
        // })
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.glimmer_video_button)?.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?){
+        when (v?.id) {
+            R.id.glimmer_video_button -> {
+                val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=egYTYpleY0U"))
+                startActivity(i)
+            }
+
+            }
+        }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_glimmer, container, false)
     }
-
 
 }
